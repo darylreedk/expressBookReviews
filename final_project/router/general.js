85656a453a2428ee7aadda7369d493e4a0ac3900
Daryl Reed Kritsch <83608143+darylreedk@users.noreply.github.com>
2023-11-08 15:28:05 -0800

    finish general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -16,16 +16,23 @@ public_users.post("/register", (req,res) => {
       } else {
         return res.status(404).json({message: "User already exists!"});    
       }
-    } 
+    }
     return res.status(404).json({message: "Unable to register user."});
 });
 
-// Get the book list available in the shop
+// Get the book list available in the shop with promise/callback functions
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify({books},null,4));
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({books});
+        }, 1000)
+    })
+    .then(data => {
+        res.send(JSON.stringify(data, null, 4))
+    })
 });
 
-// Get book details based on ISBN
+// Get book details based on ISBN with promise/callback functions
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     let filtered_isbn = null;
@@ -35,10 +42,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
             break;
         }
     }
-    res.send(filtered_isbn);
+
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({filtered_isbn})
+        }, 5000)
+    })
+    .then(data => {
+        res.send(filtered_isbn);
+    })
 });
 
-// Get book details based on author
+// Get book details based on author with promise/callback functions
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
     let filtered_author = null;
@@ -49,10 +64,17 @@ public_users.get('/author/:author',function (req, res) {
             break;
         }
     }
-    res.send(filtered_author);
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({filtered_author})
+        }, 5000)
+    })
+    .then(data => {
+        res.send(filtered_author)
+    })
 });
 
-// Get all books based on title
+// Get all books based on title with promise/ callback functions
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
     filtered_title = null;
@@ -62,10 +84,18 @@ public_users.get('/title/:title',function (req, res) {
             break;
         }
     }
-    res.send(filtered_title);
+    
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({filtered_title})
+        }, 5000)
+    })
+    .then(data => {
+        res.send(filtered_title);
+    })
 });
 
-//  Get book review using isbn
+//  Get book review using isbn with promise/callback functionality
 public_users.get('/review/:isbn',function (req, res) {
     const review = req.params.isbn;
     filtered_review = null;
@@ -75,7 +105,15 @@ public_users.get('/review/:isbn',function (req, res) {
             break;
         }
     }
-    res.send(filtered_review);
+
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({filtered_review})
+        }, 5000)
+    })
+    .then(data => {
+        res.send(filtered_review);
+    })
 });
 
 module.exports.general = public_users;
